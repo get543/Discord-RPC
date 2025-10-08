16acf324e228700463026f05ecb92b1c40da677e
get543 <fghaza.pratama@gmail.com>
2025-10-08 16:45:36 +0700

    add README & .env config

---
+++ README.md
@@ -0,0 +1,14 @@
+# DISCORD-RPC
+
+Create a custom Discord RPC
+
+# How-To
+
+1. https://discord.com/developers/applications
+2. "New Application"
+3. Create `.env` file in the same folder as `stats.js`
+
+.env template
+```env
+clientId = "1212523452345"
+```
--- stats.js
+++ stats.js
@@ -1,38 +1,43 @@
-// https://discord.com/developers/applications/944966344254816266/information
+// https://discord.com/developers/applications/<id>/information
 
-let client = new (require('discord-rpc-revamp').Client)();
-client.connect({ clientId: '944966344254816266' }).catch(console.error);
+require("dotenv").config();
 
-client.on('ready', _ => {
-    client.setActivity({
-        details: "Look at dis dood",
-        state: "Playing with ya mum",
-        startTimestamp: Date.now(),
-        largeImageKey: "dick_smith",
-        largeImageText: "Dick Smith",
-        smallImageKey: "dedi_galer",
-        smallImageText: "dedy komputer",
-        partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
-        partySize: 6,
-        partyMax: 9,
-        // joinSecret: "MTI4NzM0OjFpMmhuZToxMjMxMjM= ",
-        instance: true,
-        buttons: [
+let client = new (require("discord-rpc-revamp").Client)();
+client.connect({ clientId: process.env.clientId }).catch(console.error);
+
+client.on("ready", (_) => {
+  client
+    .setActivity({
+      details: "Look at dis dood",
+      state: "Playing with ya mum",
+      startTimestamp: Date.now(),
+      largeImageKey: "dick_smith",
+      largeImageText: "Dick Smith",
+      smallImageKey: "dedi_galer",
+      smallImageText: "dedy komputer",
+      partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
+      partySize: 6,
+      partyMax: 9,
+      // joinSecret: "MTI4NzM0OjFpMmhuZToxMjMxMjM= ",
+      instance: true,
+      buttons: [
         {
-            label: "Yutub Lebih Dari TV 🤯",
-            url: "https://www.youtube.com/@ud1n",
+          label: "Yutub Lebih Dari TV 🤯",
+          url: "https://www.youtube.com/@ud1n",
         },
         {
-            label: "The Hub 👩‍💻",
-            url: "https://github.com/get543",
+          label: "The Hub 👩‍💻",
+          url: "https://github.com/get543",
         },
-        ],
-    }).then(_ => console.log('Ready! Look at your status now :)')).catch(console.error);
+      ],
+    })
+    .then((_) => console.log("Ready! Look at your status now :)"))
+    .catch(console.error);
 
-    client.subscribe('ACTIVITY_JOIN');
-    client.subscribe('ACTIVITY_JOIN_REQUEST');
+  client.subscribe("ACTIVITY_JOIN");
+  client.subscribe("ACTIVITY_JOIN_REQUEST");
 
-    client.on('ACTIVITY_JOIN', data => {
-        console.log('ACTIVITY_JOIN', data);
-    });
+  client.on("ACTIVITY_JOIN", (data) => {
+    console.log("ACTIVITY_JOIN", data);
+  });
 });
